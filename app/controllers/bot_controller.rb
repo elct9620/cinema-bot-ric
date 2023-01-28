d13b5555ef45d5a2b4d073957adf9a75abd97893
蒼時弦也 <elct9620@frost.tw>
2023-01-29 02:01:07 +0800

    Add debug success response

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -3,7 +3,7 @@
 class BotController < ApplicationController
   class Unauthorized < StandardError; end
 
-  rescue_from Unauthorized do
+  rescue_from Unauthorized, Aws::SESV2::Errors::AccessDeniedException do
     render json: { error: :unauthorized }, status: :unauthorized
   end
 
@@ -15,5 +15,6 @@ class BotController < ApplicationController
     raise Unauthorized unless ENV['DEBUG_TOKEN'] == params['_token']
 
     NotificationMailer.debug.deliver_now
+    render json: { ok: true }
   end
 end
