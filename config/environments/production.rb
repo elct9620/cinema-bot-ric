7739a6ab1b1b8c10ef0bb431b5454bec02064572
蒼時弦也 <elct9620@frost.tw>
2023-01-29 00:44:58 +0800

    Use SES to send mail in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "cinema_bot_ric_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :sesv2
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
