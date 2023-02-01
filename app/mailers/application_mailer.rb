dbaa85094bd8556ace5356d376cd1ea0bb2537f3
蒼時弦也 <elct9620@frost.tw>
2023-02-01 17:22:53 +0800

    Add ses configuration set options

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,4 +3,12 @@
 class ApplicationMailer < ActionMailer::Base
   default from: ENV['MAILER__FROM']
   layout 'mailer'
+
+  after_action :set_tracker_headers
+
+  private
+
+  def set_tracker_headers
+    headers['X-SES-CONFIGURATION-SET'] = ENV['MAILER__CONFIGURATION_SET']
+  end
 end
